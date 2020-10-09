e87236fd6e23688fcc63a2ab901e4ce704eeac23
lunatik752 <whitemaxim752@gmail.com>
2020-10-09 11:48:17 +0300

    changed styles for component CharacterPage

--- src/components/characterPage/CharacterPage.jsx
+++ src/components/characterPage/CharacterPage.jsx
@@ -9,7 +9,7 @@ export const CharacterPage = () => {
     console.log('page')
 
     const {id} = useParams()
-    const character = useSelector(state =>  state.characterPage.character)
+    const character = useSelector(state => state.characterPage.character)
 
 
     const dispatch = useDispatch()
@@ -18,16 +18,45 @@ export const CharacterPage = () => {
         dispatch(getCharacter(id))
     }, [dispatch, id])
 
+    const characterStatus = character.status;
+    let statusClassName = '';
+    switch (characterStatus) {
+        case 'Alive':
+            statusClassName = style.characterStatusAlive;
+            break;
+        case 'Dead':
+            statusClassName = style.characterStatusDead;
+            break;
+        default:
+
+    }
+
 
     return (
-            <div className={style.container}>
+        <div className={style.container}>
+            <div className={style.characterPhoto}>
                 <img src={character.image}
-                     alt={character.name}
-                     className={style.characterPhoto}/>
+                     alt={character.name}/>
+            </div>
+
+            <div className={style.characterDescription}>
                 <div className={style.characterName}>
                     <h3>{character.name}</h3>
                 </div>
+                <div className={style.characterStatus}>
+                    <span><span className={statusClassName}>{characterStatus}</span> - {character.gender} </span>
+                </div>
+                <div className={style.description}>
+                    <span className={style.sectionTitle}> Last known location:</span>
+                    <span className={style.descriptionName}> Last known location:</span>
+                </div>
+                <div className={style.description}>
+                    <span className={style.sectionTitle}> First seen in::</span>
+                    <span className={style.descriptionName}> Last known location:</span>
+                </div>
             </div>
+
+        </div>
     );
 }
 
