375094598c9dd41b2d70f55c719fb1366fe2ed62
lunatik752 <whitemaxim752@gmail.com>
2020-10-10 14:31:02 +0300

    added tests to charactePage-reducer.test.js

--- src/state/charactePage-reducer.test.js
+++ src/state/charactePage-reducer.test.js
@@ -1,4 +1,4 @@
-import {characterPageReducer, setCharacter} from "./charactePage-reducer";
+import {characterPageReducer, clearState, setCharacter, setFirstEpisodeTitle} from "./charactePage-reducer";
 
 
 let startState = {
@@ -9,7 +9,7 @@ let startState = {
 
 test('character should be added', () => {
 
-    const newStatus =  {
+    const newCharacter = {
         id: 2,
         name: "Morty Smith",
         status: "Alive",
@@ -33,10 +33,62 @@ test('character should be added', () => {
         url: "https://rickandmortyapi.com/api/character/2",
         created: "2017-11-04T18:50:21.651Z"
     }
-    const action = setCharacter(newStatus);
+
+    const action = setCharacter(newCharacter);
     const endState = characterPageReducer(startState, action)
 
-    expect(endState.status).toBe('loading');
+    expect(endState.character.id).toBe(2);
+    expect(endState.character.episode.length).toBe(3);
+
 });
 
 
+test('firstEpisodeTitle should be added', () => {
+
+    const newEpisodeTitle = {
+        name: 'Rixty Minutes'
+    }
+
+    const action = setFirstEpisodeTitle(newEpisodeTitle);
+    const endState = characterPageReducer(startState, action)
+
+    expect(endState.firstEpisodeTitle).toBe(newEpisodeTitle.name);
+
+});
+
+test('state must be cleared', () => {
+
+    const newStartState = {
+        character: {
+            id: 2,
+            name: "Morty Smith",
+            status: "Alive",
+            species: "Human",
+            type: "",
+            gender: "Male",
+            origin: {
+                name: "Earth (C-137)",
+                url: "https://rickandmortyapi.com/api/location/1"
+            },
+            location: {
+                name: "Earth (Replacement Dimension)",
+                url: "https://rickandmortyapi.com/api/location/20"
+            },
+            image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+            episode: [
+                "https://rickandmortyapi.com/api/episode/1",
+                "https://rickandmortyapi.com/api/episode/2",
+                "https://rickandmortyapi.com/api/episode/3",
+            ],
+            url: "https://rickandmortyapi.com/api/character/2",
+            created: "2017-11-04T18:50:21.651Z"
+        },
+        firstEpisodeTitle: 'Rixty Minutes'
+    }
+
+    const action = clearState(newStartState);
+    const endState = characterPageReducer(newStartState, action)
+
+    expect(endState.firstEpisodeTitle).toBe(startState.firstEpisodeTitle);
+    expect(typeof endState.character['name']).toBe("undefined");
+});
