08fd331f5fb73daca5ec78933da05ea13a933559
lunatik752 <whitemaxim752@gmail.com>
2020-10-22 20:46:42 +0300

    fixed charactersList-reducer.test.js

--- src/components/charactersList/charactersList-reducer.test.js
+++ src/components/charactersList/charactersList-reducer.test.js
@@ -1,4 +1,5 @@
-import {charactersListReducer, setCharactersList} from "./charactersList-reducer";
+import {charactersListReducer} from "./charactersList-reducer";
+import {charactersListActions} from "./index";
 
 
 let startState = {
@@ -9,61 +10,62 @@ let startState = {
 
 test('data should be added', () => {
 
-const newCharactersList = {
-    info: {
-        count: 671,
-        pages: 34,
-        next: "https://rickandmortyapi.com/api/character/?page=4",
-        prev: "https://rickandmortyapi.com/api/character/?page=2" },
-    results: [
-        {
-            id: 42,
-            name: "Big Head Morty",
-            status: "unknown",
-            species: "Human",
-            type: "Human with giant head",
-            gender: "Male",
-            origin: {
-                name: "unknown",
-                url: ""
-            },
-            location: {
-                name: "Citadel of Ricks",
-                url: "https://rickandmortyapi.com/api/location/3"
-            },
-            image: "https://rickandmortyapi.com/api/character/avatar/42.jpeg",
-            episode: [
-                "https://rickandmortyapi.com/api/episode/22"
-            ],
-            url: "https://rickandmortyapi.com/api/character/42",
-            created: "2017-11-05T10:15:53.349Z"
+    const newCharactersList = {
+        info: {
+            count: 671,
+            pages: 34,
+            next: "https://rickandmortyapi.com/api/character/?page=4",
+            prev: "https://rickandmortyapi.com/api/character/?page=2"
         },
-        {
-            id: 43,
-            name: "Big Head Morty",
-            status: "unknown",
-            species: "Human",
-            type: "Human with giant head",
-            gender: "Male",
-            origin: {
-                name: "unknown",
-                url: ""
+        results: [
+            {
+                id: 42,
+                name: "Big Head Morty",
+                status: "unknown",
+                species: "Human",
+                type: "Human with giant head",
+                gender: "Male",
+                origin: {
+                    name: "unknown",
+                    url: ""
+                },
+                location: {
+                    name: "Citadel of Ricks",
+                    url: "https://rickandmortyapi.com/api/location/3"
+                },
+                image: "https://rickandmortyapi.com/api/character/avatar/42.jpeg",
+                episode: [
+                    "https://rickandmortyapi.com/api/episode/22"
+                ],
+                url: "https://rickandmortyapi.com/api/character/42",
+                created: "2017-11-05T10:15:53.349Z"
             },
-            location: {
-                name: "Citadel of Ricks",
-                url: "https://rickandmortyapi.com/api/location/3"
-            },
-            image: "https://rickandmortyapi.com/api/character/avatar/42.jpeg",
-            episode: [
-                "https://rickandmortyapi.com/api/episode/22"
-            ],
-            url: "https://rickandmortyapi.com/api/character/42",
-            created: "2017-11-05T10:15:53.349Z"
-        }
-    ]
-}
+            {
+                id: 43,
+                name: "Big Head Morty",
+                status: "unknown",
+                species: "Human",
+                type: "Human with giant head",
+                gender: "Male",
+                origin: {
+                    name: "unknown",
+                    url: ""
+                },
+                location: {
+                    name: "Citadel of Ricks",
+                    url: "https://rickandmortyapi.com/api/location/3"
+                },
+                image: "https://rickandmortyapi.com/api/character/avatar/42.jpeg",
+                episode: [
+                    "https://rickandmortyapi.com/api/episode/22"
+                ],
+                url: "https://rickandmortyapi.com/api/character/42",
+                created: "2017-11-05T10:15:53.349Z"
+            }
+        ]
+    }
 
-    const action = setCharactersList(newCharactersList);
+    const action = charactersListActions.setCharactersList(newCharactersList);
     const endState = charactersListReducer(startState, action)
 
     expect(endState.info.pages).toBe(34);
